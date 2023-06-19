1460f41978c59dc583ae7e030a48b6505c0ec398
wr9263 <wr9263@student.wsb.wroclaw.pl>
2023-06-19 18:43:33 +0200

    Dodano zmiany w Dockerfile, app.py i requirements.txt

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 COPY . .
-CMD [“python3”, “-m”, “flask”, “run”, “--host=0.0.0.0”]
+CMD ["python3", "-m", "flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-"""
-This is a simple Flask application.
-"""
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """
-    Returns a simple HTML greeting.
-    """
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
